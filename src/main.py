42eec5f9c806c3ff6fda0323f56bb3f3b25348c9
Eduardo Gravan <eduardogravan@gmail.com>
2021-07-03 18:28:22 +0200

    added test gui

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-from TwitterHandler import *
+from TwitterHandler import TwitterHandler
+from test_interfaz import Ui_Dialog
 
 def main():
     foo = TwitterHandler()
     foo.test()
+    
+    Ui_Dialog()
 
 if __name__ == '__main__':
     main()
